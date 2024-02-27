2ec79b25b86788c8e93b9fa12d8db823983a6b6b
serdjukow <serduykovs@yandex.ru>
2024-02-27 08:05:53 +0100

    Update - konnektoren page

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,19 @@
+import React, { useRef } from 'react';
 import ConnectorCard from "../ConnectorCard/ConnectorCard.jsx";
 import './modal.scss'
 
 
 const Modal = (props) => {
+    const wrapperRef = useRef(null);
+    const handleClick = (event) => {
+        if (event.target === wrapperRef.current) {
+            props.chengeIsOpen()
+        }
+    }
 
     return (
         <div className={`modal ${props.isOpen && 'open'}`}>
-            <div className="modal__wrapper">
+            <div className="modal__wrapper" ref={wrapperRef} onClick={handleClick}>
                 <div className="modal__body">
                     <div className="modal__header">
                         <span onClick={props.chengeIsOpen} className="modal__close">⛌</span>
@@ -14,7 +21,7 @@ const Modal = (props) => {
                     <ConnectorCard connector={props.modalContent} />
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
